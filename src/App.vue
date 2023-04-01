1b2e1c5759fe7e928f4063917e1ddece7577636b
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-04-01 17:16:17 -0300

    Mandimento das primeiras versoes e inicio do BT5

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <script setup>
 
-import { ref, computed } from 'vue'
+import { ref, } from 'vue'
 import estados from '@/_data/estados'
-
-// transformar tudo isso em user e colocar o valor no user 
-
 
 const user = ref({
   nome: '',
@@ -14,86 +11,115 @@ const user = ref({
   data: 0,
   endereco: '',
   cidade: '',
-  cidade: '',
   estado: '',
   hobbies: '',
-  linguagens: [],
-  biografia:'',
+  linguagens: ([]),
+  biografia: '',
 })
 const newLang = ref('')
+
+// PRIMEIRA VERSÃO MANDITA PARA ESTUDOS--
 //const nome = ref('')
 //const email = ref('')
 //const senha = ref('')
 //const confirma = ref('')
-const data = ref('')
+// const data = ref('')
 //const endereco = ref('')
 //const cidade = ref('')
-const estado = ref([])
+//const estado = ref([])
 //const hobbies = ref('')
 //const linguagens = ref('')
 //const biografia = ref('')
 
 
-
 </script>
 
 <template>
-  <div class="container-lg">
-    <div><input type="text" v-model="user.name" placeholder="Digite seu nome" />{{ user.name }}</div>
-    <div><input type="text" v-model="email" placeholder="Digite seu e-mail" />{{ email }}</div>
-    <div><input type="password" v-model="senha" placeholder="Digite sua senha" /> {{ senha }}</div>
-    <div><input type="password" v-model="confirma" placeholder="Confirme sua senha" /> {{ confirma }}</div>
-    <div><input type="datetime-local" v-model="data" placeholder="Data de nascimento" /> {{ data }}</div>
-    <div><input type="text" v-model="endereco" placeholder="Endereço" /> {{ endereco }}</div>
-    <div><input type="text" v-model="cidade" placeholder="Cidade" /> {{ cidade }} </div>
-    <div><input type="select" v-model="estado" placeholder="Qual seu estado?" /> Estados:
-      <select v-model="estado">
-        <option v-for="estado of estados" :key="estado.sigla" :value="estado.sigla">{{ estado.nome }}</option>
-        <!-- <option value="AC">Acre</option>
-        <option value="AL">Alagoas</option>
-        <option value="AP">Amapá</option>
-        <option value="AM">Amazonas</option>
-        <option value="BA">Bahia</option>
-        <option value="CE">Ceara</option>
-        <option value="DF">Distrito Federal</option>
-        <option value="ES">Espirito Santo</option>
-        <option value="GO">Goias</option>
-        <option value="MA">Maranhão</option>
-        <option value="Mato Grosso">MT</option>
-        <option value="Mato Grosso do Sul">MS</option>
-        <option value="Minas Gerais">MG</option>
-        <option value="Pará">PA</option>
-        <option value="Paraíba">PB</option>
-        <option value="Paraná">PR</option>
-        <option value="Pernambuco">PE</option>
-        <option value="Piauí">PI</option>
-        <option value="Rio de Janeiro">RJ</option>
-        <option value="Rio Grande do Norte">RN</option>
-        <option value="Rio Grande do Sul">RS</option>
-        <option value="Rondonia">RO</option>
-        <option value="Roraima">RR</option>
-        <option value="Santa Catarina">SC</option>
-        <option value="São Paulo">SP</option>
-        <option value="Sergipe">SE</option>
-        <option value="Tocantins">TO</option> -->
-      </select>
-    </div>
-    <div>
-      <h3>Site alguns hobbies</h3>
-      <textarea v-model="hobbies" />
-      {{ hobbies }}
-    </div>
-    <div>
-      <input type="text" v-model="newLang" @keypress.enter="user.linguagens.push(newLang); newLang=''" placeholder="Linguagens prog" /> 
-      {{ user.linguagens }}
-    </div>
-    <div>
-      <h3>Faça uma autobiografia</h3>
-      <textarea v-model="biografia" />
-      {{ biografia }}
-    </div>
+  <div class="container-fluid">
+  <form>
+    <div class="form-row">
+      <div class="col-sm-4">
+        <input type="text" v-model="user.name" class="form-control" placeholder="Digite seu nome" />{{
+          user.name }}
+      </div>
+      <div class="col-sm-4">
+        <input type="text" v-model="user.email" class="form-control" placeholder="Digite seu e-mail" />{{
+          user.email }}
+      </div>
+      <div class="col-sm-4">
+        <input type="password" v-model="user.senha" class="form-control" placeholder="Digite sua senha" />  
+        {{ user.senha }}
+      </div>
+      <div class="col-sm-4">
+        <input type="password" v-model="user.confirma" class="form-control" placeholder="Confirme sua senha" /> 
+       {{ user.confirma }}
+      </div>
+      <div class="col-sm-4">
+        <input type="datetime-local" v-model="user.data" class="form-control"  placeholder="Data de nascimento" />
+         </div>
+        <div class="col-sm-4">
+          <input type="text" v-model="user.endereco" class="form-control" placeholder="Endereço" /> {{
+          user.endereco }}
+          </div>
+        <div class="col-sm-4">
+          <input type="text" v-model="user.cidade" class="form-control" placeholder="Cidade" /> 
+          {{ user.cidade }} 
+        </div>
+        <div> Estados:
+          <select class="form-select " v-model="estado">
+            <option v-for="estado of estados" :key="estado.sigla" :value="estado.sigla">{{ estado.nome }}</option>
 
+            <!-- PRIMEIRA VERSÃO MANDITA PARA ESTUDOS-- 
+                <option value="AC">Acre</option>
+              <option value="AL">Alagoas</option>
+              <option value="AP">Amapá</option>
+              <option value="AM">Amazonas</option>
+              <option value="BA">Bahia</option>
+              <option value="CE">Ceara</option>
+              <option value="DF">Distrito Federal</option>
+              <option value="ES">Espirito Santo</option>
+              <option value="GO">Goias</option>
+              <option value="MA">Maranhão</option>
+              <option value="Mato Grosso">MT</option>
+              <option value="Mato Grosso do Sul">MS</option>
+              <option value="Minas Gerais">MG</option>
+              <option value="Pará">PA</option>
+              <option value="Paraíba">PB</option>
+              <option value="Paraná">PR</option>
+              <option value="Pernambuco">PE</option>
+              <option value="Piauí">PI</option>
+              <option value="Rio de Janeiro">RJ</option>
+              <option value="Rio Grande do Norte">RN</option>
+              <option value="Rio Grande do Sul">RS</option>
+              <option value="Rondonia">RO</option>
+              <option value="Roraima">RR</option>
+              <option value="Santa Catarina">SC</option>
+              <option value="São Paulo">SP</option>
+              <option value="Sergipe">SE</option>
+              <option value="Tocantins">TO</option> -->
+
+          </select>
+        </div>
+        <div>
+          <h3>Site alguns hobbies</h3>
+          <textarea v-model="user.hobbies" />
+          {{ user.hobbies }}
+        </div>
+        <div>
+          <input type="text" v-model="newLang" @keypress.enter="user.linguagens.push(newLang); newLang = ''"
+            placeholder="Linguagens prog" />
+          {{ user.linguagens }}
+        </div>
+        <div>
+          <h3>Faça uma autobiografia</h3>
+          <textarea v-model="user.biografia" />
+          {{ user.biografia }}
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
